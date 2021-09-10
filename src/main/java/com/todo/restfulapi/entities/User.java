2c2d7c4694688506f9fcdf021c779945f84cd3ba
lehuunhan <nhan123615@gmail.com>
2021-09-10 13:46:26 +0700

    update entity schema

--- src/main/java/com/todo/restfulapi/entities/User.java
+++ src/main/java/com/todo/restfulapi/entities/User.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "user",,schema = "public")
+@Table(name = "user",schema = "public")
 @Data // lombok
 @NoArgsConstructor
 public class User {
